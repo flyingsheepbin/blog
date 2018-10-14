f4bb9125f418e9816c3f663a5f0e22a1f100aa89
flyingsheep <44082512+flyingsheepbin@users.noreply.github.com>
2018-10-14 09:51:59 +0800

    Update sql.sql
    update once

--- sql.sql
+++ sql.sql
@@ -1,8 +1,9 @@
 create database blog DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 use blog;
 create table article(
-	int id primary key auto_increment,
-	varchar(50) title,
-	varchar(200) descriable,
-	varchar(10000) page,
-	date article_post_time))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	id int  primary key auto_increment,
+	title varchar(50) ,
+	descriable varchar(200) ,
+	page varchar(10000) ,
+	article_post_time date ))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+insert article value(1,'title','descriable','page content','2017-1-1');
