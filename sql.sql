503e0a595dbc17bf6d24fb1d34875c75a6d6da63
flyingsheepbin <flyingsheep.cn@gmail.com>
2018-10-14 10:52:04 +0800

    flyingsheep

--- sql.sql
+++ sql.sql
@@ -1,8 +1,13 @@
 create database blog DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 use blog;
 create table article(
-	int id primary key auto_increment,
-	varchar(50) title,
-	varchar(200) descriable,
-	varchar(10000) page,
-	date article_post_time))ENGINE=InnoDB DEFAULT CHARSET=utf8;
+	id int  primary key auto_increment,
+	title varchar(50) ,
+	descriable varchar(200) ,
+	page varchar(10000) ,
+	article_post_time date )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table article_user(
+	id int primary key auto_increment,
+	username varchar(20),
+	password varchar(30)
+)engine=InnoDB default charset=utf8;
