dde96614e2d67db4b0ee6abfb3598de04c2b9e6f
flyingsheepbin <flyingsheep.cn@gmail.com>
2018-10-15 16:42:10 +0800

    flyingsheep

--- sql.sql
+++ sql.sql
@@ -1,7 +1,20 @@
-
+create database blog DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+use blog;
 create table article(
+	id int  primary key auto_increment,
+	title varchar(50) ,
 	id int primary key auto_increment,
-	title varchar(50), 
+	title varchar(50),
 	descriable varchar(200) ,
 	page varchar(10000) ,
 	article_post_time date )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+create table article_user(
+	id int primary key auto_increment,
+	username varchar(20),
+	password varchar(30)
+)engine=InnoDB default charset=utf8;
+alter table article add article_view_count int default 0;
+alter table article add article_comment_count int default 0;
+alter table article_user add email varchar(30);
+#给username添加唯一约束
+alter table article_user add unique(username);
--- web/WEB-INF/view/home/index.jsp
+++ web/WEB-INF/view/home/index.jsp
@@ -130,6 +130,9 @@
                 </div>
             </div>
         </div>
+        <footer>
+            <
+        </footer>
         <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
